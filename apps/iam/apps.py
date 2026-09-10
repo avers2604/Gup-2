4a71a95bf7ec2c990ffb0c5998fe11809c02b5df
avers2604 <averkiev2604@gmail.com>
2026-09-10 18:16:50 +0300

    Load IAM event handlers on app startup

--- apps/iam/apps.py
+++ apps/iam/apps.py
@@ -5,3 +5,6 @@ class IamConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.iam"
     verbose_name = "Учётные записи и оргструктура"
+
+    def ready(self):
+        from . import handlers  # noqa: F401
