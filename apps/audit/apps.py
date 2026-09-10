f08181b4e384abca2a21b410063c4e6d47198f8a
avers2604 <averkiev2604@gmail.com>
2026-09-10 18:16:46 +0300

    Load audit event handlers on app startup

--- apps/audit/apps.py
+++ apps/audit/apps.py
@@ -5,3 +5,6 @@ class AuditConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.audit"
     verbose_name = "Аудит"
+
+    def ready(self):
+        from . import handlers  # noqa: F401
